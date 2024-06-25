53cbbae05436f6987b9baf3b2c5779bc223e21c0
Amirdgan-574 <159114812+Amirdgan-574@users.noreply.github.com>
2024-06-25 04:38:42 +0300

    Update Gadget.dart

--- lib/Pages/Gadget.dart
+++ lib/Pages/Gadget.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/Pages/user.dart';
+import 'package:untitled/Pages/Gadget.dart';
+import 'package:untitled/Pages/initialPage.dart';
+import 'package:untitled/Pages/LoginPage.dart';
+import 'package:untitled/Pages/GadgetsPage.dart';
 import 'package:untitled/Pages/Client.dart';
 class Gadget {
   final String title;
+  final String IdGadgetG;
   final String photo;
+  final DateTime rentalPeriod; // Используем int для rentalPeriod
+  final double rentalCost;
+  final int dayarend;// Используем double для rentalCost
   final List<Client> clients;
-  Gadget({required this.title, required this.photo,required this.clients});
+  final String worker;// Используем List<Client> для хранения клиентов
+
+  void rent(Client client) {
+    clients.add(client); // Добавляем клиента в список клиентов гаджета
+  }
+
+  Gadget({
+    required this.title,
+    required this.photo,
+    this.clients = const [],
+    required this.IdGadgetG,
+    required this.rentalPeriod,
+    required this.rentalCost,
+    required this.dayarend,
+    required this.worker
+  });
 }
