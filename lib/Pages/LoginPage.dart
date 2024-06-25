5590ed2b10f3697ba7e1da99574dcbff6a1b0320
Amirdgan-574 <159114812+Amirdgan-574@users.noreply.github.com>
2024-06-25 04:41:54 +0300

    Update LoginPage.dart

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/Pages/GadgetsPage.dart';
+import 'package:untitled/Pages/InitialPage.dart';
 import 'package:untitled/main.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
+
 class _LoginPageState extends State<LoginPage> {
   late String _username;
   late String _password;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
-  Widget build(BuildContext context) {return Scaffold(
+  Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
-      title: Text('Вход', style: TextStyle(color: Colors.black)),
+      title: Text('Вход', style: TextStyle(color: Colors.white)),
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => InitialPage()));
+        },
+      ),
+      backgroundColor: Colors.deepOrangeAccent,
+      bottom: PreferredSize(
+        preferredSize: Size.fromHeight(4.0),
+        child: Container(),
+      ),
     ),
     backgroundColor: Theme.of(context).scaffoldBackgroundColor,
     body: Center(
@@ -24,7 +40,16 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               TextFormField(
-                decoration: InputDecoration(labelText: 'Имя пользователя'),
+                decoration: InputDecoration(
+                  labelText: 'Имя пользователя',
+                  labelStyle: TextStyle(color: Colors.black),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepOrangeAccent),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepOrangeAccent),
+                  ),
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Пожалуйста, введите имя пользователя';
@@ -37,7 +62,16 @@ class _LoginPageState extends State<LoginPage> {
               ),
               SizedBox(height: 10),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Пароль'),
+                decoration: InputDecoration(
+                  labelText: 'Пароль',
+                  labelStyle: TextStyle(color: Colors.black),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepOrangeAccent),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepOrangeAccent),
+                  ),
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Пожалуйста, введите пароль';
@@ -54,12 +88,16 @@ class _LoginPageState extends State<LoginPage> {
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
                     _formKey.currentState!.save();
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => GadgetsPage()));
                   }
                 },
                 child: Text('Войти', style: TextStyle(color: Colors.white)),
                 style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                  backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.deepOrangeAccent),
                 ),
               ),
             ],
@@ -68,5 +106,4 @@ class _LoginPageState extends State<LoginPage> {
       ),
     ),
   );
-  }
 }
